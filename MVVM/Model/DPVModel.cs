b65ac6cedf6114d104556db60f709e59640c4170
Jon Micah M- <99670836+MillerSty@users.noreply.github.com>
2023-12-24 18:25:54 -0500

    Fuck, working on experiment checkboxes
    Will switch to new branch but its here now

---
+++ MVVM/Model/DPVModel.cs
@@ -0,0 +1,22 @@
+﻿namespace C490_App.MVVM.Model
+{
+    public class DPVModel
+    {
+        public bool isEnabled { get; set; }
+        public double startVoltage { get; set; }
+        public double voltageThresholdOne { get; set; }
+        public double voltageThresholdTwo { get; set; }
+        public double stepSize { get; set; }
+        public double scanRate { get; set; }
+        public double numOfScans { get; set; }
+
+
+        public DPVModel()
+        {
+
+        }
+
+
+
+    }
+}
--- MVVM/ViewModel/HomeFrameViewModel.cs
+++ MVVM/ViewModel/HomeFrameViewModel.cs
@@ -1,15 +1,35 @@
-﻿namespace C490_App.MVVM.ViewModel
+﻿using C490_App.Core;
+using C490_App.MVVM.Model;
+
+namespace C490_App.MVVM.ViewModel
 {
     public class HomeFrameViewModel
     {
 
         public PotentiostatViewModel PotentiostatViewModel { get; }
+
         public LedArrayViewModel LedArrayViewModel { get; }
+        public CAModel _caModel { get; }
+        public DPVModel _dpvModel { get; }
+        public CVModel _cvModel { get; }
+        public RelayCommand check { get; }
+
+
 
         public HomeFrameViewModel()
         {
+            _caModel = new CAModel();
+            _dpvModel = new DPVModel();
+            _cvModel = new CVModel();
             PotentiostatViewModel = new PotentiostatViewModel();
             LedArrayViewModel = new LedArrayViewModel();
+
+            check = new RelayCommand(o => newStore(), o => true);
+        }
+        public void newStore()
+        {
+
+            int check = 100;
         }
 
 
