afd8986f98d7cb0cd82b61cb3d8da64293cd0c25
Jon Micah M- <99670836+MillerSty@users.noreply.github.com>
2023-12-28 19:19:01 -0500

    LEDArrayViewModel updated
    LEDArrayViewModel updated with experimentStore

--- MVVM/ViewModel/LedArrayViewModel.cs
+++ MVVM/ViewModel/LedArrayViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using C490_App.Core;
-using C490_App.MVVM.Model;
 using C490_App.Services;
 using System.Collections.ObjectModel;
 
@@ -11,20 +10,16 @@ namespace C490_App.MVVM.ViewModel
     {
 
         public RelayCommand ledSelect { get; set; }
-        public RelayCommand doNothing { get; set; }
         public bool controlCheck { get; set; }
 
-
-        private ObservableCollection<LEDParameter> lEDParameters { get; set; }
         public ObservableCollection<bool> isSelected { get; set; }
-        ObservableCollection<LEDParameter> ledParameters { get; set; }
 
         ExperimentStore ExperimentLocal { get; set; }
+
         public LedArrayViewModel(ExperimentStore ExperimentSingleton)
         {
             ExperimentLocal = ExperimentSingleton;
-            isSelected = new ObservableCollection<bool>();
-            ledParameters = new ObservableCollection<LEDParameter>(initLedArray());
+            isSelected = new ObservableCollection<bool>(initLedArray()); //this could probably be handled more eloquently
             ledSelect = new RelayCommand(o => SelectLed(o), o => true);
         }
 
@@ -44,8 +39,8 @@ namespace C490_App.MVVM.ViewModel
                 int k = int.Parse(parameterSubString[0]);
                 for (int i = 0; i < 10; i++)
                 {
-                    bool set = !ledParameters[k].isSelected;
-                    this.ledParameters[k].isSelected = set;
+                    bool set = !ExperimentLocal.ledParameters[k].isSelected;
+                    this.ExperimentLocal.ledParameters[k].isSelected = set;
                     this.isSelected[k] = set;
                     k += int.Parse(parameterSubString[1]);
                 }
@@ -54,26 +49,23 @@ namespace C490_App.MVVM.ViewModel
             {
                 for (int i = int.Parse(parameterSubString[0]); i < int.Parse(parameterSubString[1]); i++)
                 {
-                    bool set = !ledParameters[i].isSelected;
-                    this.ledParameters[i].isSelected = set;
+                    bool set = !ExperimentLocal.ledParameters[i].isSelected;
+                    this.ExperimentLocal.ledParameters[i].isSelected = set;
                     this.isSelected[i] = set;
                 }
             }
-            //this is to test datapassing 
-            ExperimentLocal.ledParameters = ledParameters;
         }
 
-        internal List<LEDParameter> initLedArray()
+        internal List<bool> initLedArray()
         {
-            List<LEDParameter> ledParameters = new List<LEDParameter>();
+            List<bool> result = new List<bool>();
 
             for (int i = 0; i < 50; i++)
             {
-                ledParameters.Add(new LEDParameter(false, Convert.ToUInt32(i)));
-                this.isSelected.Add(false);
+                result.Add(false);
             }
 
-            return ledParameters;
+            return result;
         }
 
     }
