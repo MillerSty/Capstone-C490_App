c16a0e5c6a534751c12236b84c9a1bd493f7b76d
Kas73 <kasrakamjoo@live.com>
2024-01-10 11:31:16 -0500

    read csv files and load them onto graphFrame

---
+++ MVVM/Model/CsvReadingsModel.cs
@@ -0,0 +1,23 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C490_App.MVVM.Model
+{
+    internal class CsvReadingsModel
+    {
+        private string csvFilePath;
+
+        public CsvReadingsModel(string csvFilePath)
+        {
+            this.csvFilePath = csvFilePath;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public PlotModel? PlotModel { get; internal set; }
+    }
+}
--- MVVM/View/GraphFrame.xaml.cs
+++ MVVM/View/GraphFrame.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using C490_App.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +25,48 @@ namespace C490_App.MVVM.View
         public GraphFrame()
         {
             InitializeComponent();
+            LoadCsvFileNames();
         }
+
+        private void LoadCsvFileNames()
+        {
+            string appFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory.Split(new[] { "\\bin\\" }, StringSplitOptions.None)[0], "Resources", "CSV Readings");
+
+            string[] csvFiles = Directory.GetFiles(appFolder, "*.csv");
+
+            // Populate the ListBox with file names
+            foreach (string csvFile in csvFiles)
+            {
+                csvListBox.Items.Add(System.IO.Path.GetFileNameWithoutExtension(csvFile));
+            }
+        }
+
+        private void CsvListBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            // Handle the selection change
+            string? selectedCsvFileName = csvListBox.SelectedItem as string;
+
+            if (!string.IsNullOrEmpty(selectedCsvFileName))
+            {
+                // Load and plot the selected CSV file
+                PlotCsvFile(selectedCsvFileName);
+            }
+        }
+
+        private void PlotCsvFile(string csvFileName)
+        {
+            // Clear the plot
+            plotView.Model = null;
+
+            // Load the CSV file
+            string appFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory.Split(new[] { "\\bin\\" }, StringSplitOptions.None)[0], "Resources", "CSV Readings");
+            string csvFilePath = appFolder + csvFileName + ".csv";
+
+            // Read the CSV file
+            var csvFile = new CsvReadingsModel(csvFilePath);
+
+            // Plot the CSV file
+            plotView.Model = csvFile.PlotModel;
+        }   
     }
 }
