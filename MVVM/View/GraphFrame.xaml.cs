6fb60b3ba3fbaf54d82c572c14c9846050b131c3
Kas73 <kasrakamjoo@live.com>
2024-01-14 11:50:33 -0500

    Moved oxyplot code from View to ViewModel

--- MVVM/View/GraphFrame.xaml.cs
+++ MVVM/View/GraphFrame.xaml.cs
@@ -1,102 +1,17 @@
-﻿using C490_App.MVVM.Model;
-using OxyPlot.Axes;
-using OxyPlot;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using C490_App.MVVM.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using CsvHelper;
-using OxyPlot.Series;
-using System.Globalization;
 
 namespace C490_App.MVVM.View
 {
-    /// <summary>
-    /// Interaction logic for GraphFrame.xaml
-    /// </summary>
     public partial class GraphFrame : Window
     {
-        private PlotModel plotModel;
+        //private GraphFrameViewModel viewModel;
+
         public GraphFrame()
         {
             InitializeComponent();
-            LoadCsvFileNames();
-            InitializePlotModel();
+            //viewModel = new GraphFrameViewModel();
+            //DataContext = viewModel;
         }
-
-        private void InitializePlotModel()
-        {
-            plotModel = new PlotModel();
-            plotView.Model = plotModel;
-
-            
-            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "X-Axis" });
-            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Y-Axis" });
-        }
-
-        private void LoadCsvFileNames()
-        {
-            string appFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory.Split(new[] { "\\bin\\" }, StringSplitOptions.None)[0], "Resources", "CSV Readings");
-
-            string[] csvFiles = Directory.GetFiles(appFolder, "*.csv");
-
-            // Populate the ListBox with file names
-            foreach (string csvFile in csvFiles)
-            {
-                csvListBox.Items.Add(System.IO.Path.GetFileNameWithoutExtension(csvFile));
-            }
-        }
-
-        private void CsvListBox_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-            // Handle the selection change
-            string? selectedCsvFileName = csvListBox.SelectedItem as string;
-
-            if (!string.IsNullOrEmpty(selectedCsvFileName))
-            {
-                // Load and plot the selected CSV file
-                PlotCsvFile(selectedCsvFileName);
-            }
-        }
-        private void PlotCsvFile(string csvFileName)
-        {
-            string appFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory.Split(new[] { "\\bin\\" }, StringSplitOptions.None)[0], "Resources", "CSV Readings");
-            string csvFilePath = System.IO.Path.Combine(appFolder, $"{csvFileName}.csv");
-
-            using (var reader = new StreamReader(csvFilePath))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                // CSV has two columns: X and Y
-                var records = csv.GetRecords<CsvReadingsModel>();
-
-
-                // Create a LineSeries to plot the data
-                var lineSeries = new LineSeries();
-
-                foreach (var record in records)
-                {
-                    lineSeries.Points.Add(new DataPoint(record.X, record.Y));
-                }
-
-                // Add the new series to the existing plot model
-                plotModel.Series.Add(lineSeries);
-
-                // Refresh the plot
-                plotModel.InvalidatePlot(true);
-            }
-        }
-
-
     }
 }
---
+++ MVVM/ViewModel/GraphFrameViewModel.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
+using C490_App.Core;
+using C490_App.MVVM.Model;
+using CsvHelper;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace C490_App.MVVM.ViewModel
+{
+    public class GraphFrameViewModel : ViewModelBase
+    {
+        private PlotModel plotModel;
+        private ObservableCollection<string> csvFileNames;
+        private string selectedCsvFileName;
+
+        public PlotModel PlotModel
+        {
+            get { return plotModel; }
+            set
+            {
+                if (plotModel != value)
+                {
+                    plotModel = value;
+                    OnPropertyChanged(nameof(PlotModel));
+                }
+            }
+        }
+
+        public ObservableCollection<string> CsvFileNames
+        {
+            get { return csvFileNames; }
+            set
+            {
+                if (csvFileNames != value)
+                {
+                    csvFileNames = value;
+                    OnPropertyChanged(nameof(CsvFileNames));
+                }
+            }
+        }
+
+        public string SelectedCsvFileName
+        {
+            get { return selectedCsvFileName; }
+            set
+            {
+                if (selectedCsvFileName != value)
+                {
+                    selectedCsvFileName = value;
+                    OnPropertyChanged(nameof(SelectedCsvFileName));
+
+                    // Load and plot the selected CSV file
+                    PlotCsvFile(selectedCsvFileName);
+                }
+            }
+        }
+
+        public ICommand LoadSelectedCsvCommand { get; private set; }
+
+        public GraphFrameViewModel()
+        {
+            LoadCsvFileNames();
+            InitializePlotModel();
+            LoadSelectedCsvCommand = new RelayCommand(ExecuteLoadSelectedCsv, CanExecuteLoadSelectedCsv);
+        }
+
+        private void InitializePlotModel()
+        {
+            PlotModel = new PlotModel();
+
+            PlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "X-Axis" });
+            PlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Y-Axis" });
+        }
+
+        private void LoadCsvFileNames()
+        {
+            string appFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory.Split(new[] { "\\bin\\" }, StringSplitOptions.None)[0], "Resources", "CSV Readings");
+
+            string[] csvFiles = Directory.GetFiles(appFolder, "*.csv");
+
+            CsvFileNames = new ObservableCollection<string>(csvFiles.Select(csvFile => Path.GetFileNameWithoutExtension(csvFile)));
+        }
+
+        private void ExecuteLoadSelectedCsv(object parameter)
+        {
+            // Handle the execution of the LoadSelectedCsvCommand
+            PlotCsvFile(SelectedCsvFileName);
+        }
+
+        private bool CanExecuteLoadSelectedCsv(object parameter)
+        {
+            // You can add conditions for whether the command can be executed or not
+            return !string.IsNullOrEmpty(SelectedCsvFileName);
+        }
+
+        public void PlotCsvFile(string csvFileName)
+        {
+            string appFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory.Split(new[] { "\\bin\\" }, StringSplitOptions.None)[0], "Resources", "CSV Readings");
+            string csvFilePath = Path.Combine(appFolder, $"{csvFileName}.csv");
+
+            using (var reader = new StreamReader(csvFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<CsvReadingsModel>();
+
+                var lineSeries = new LineSeries();
+
+                foreach (var record in records)
+                {
+                    lineSeries.Points.Add(new DataPoint(record.X, record.Y));
+                }
+
+                PlotModel.Series.Add(lineSeries);
+                PlotModel.InvalidatePlot(true);
+            }
+        }
+    }
+}
